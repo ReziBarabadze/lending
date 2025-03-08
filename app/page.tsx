53344bb41036139299d9deb334cc24381c220b16
Rezi <rezibarabadze153@gmail.com>
2025-03-08 18:10:53 +0400

    add new webpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,48 @@
+import Email from "@/components/Email";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        src="images/logo.svg"
+        alt="logo"
+        width={90}
+        height={29}
+        style={{ margin: "35px" }}
+      />
+      <Typography variant="h2" sx={{ color: "#969696", fontWeight: 400 }}>
+        We are launching{" "}
+        <span style={{ color: "#15202a", fontWeight: 700 }}>soon!</span>
+      </Typography>
+      <Typography
+        sx={{
+          color: "#15202a",
+          fontSize: "1.25rem",
+          marginTop: "0.5rem",
+          marginBottom: "1.5rem",
+        }}
+      >
+        Subscribe and get notified
+      </Typography>
+      <Email />
+      <Image
+        src="/images/illustration-dashboard.png"
+        alt="image"
+        width={600}
+        height={350}
+      />
+    </Box>
   );
-}
+};
+
+export default Home;
---
+++ components/Email.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Box, Button, Input, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+const Email = () => {
+  const [isValid, setIsValid] = useState(true);
+  const isEmail = (email: string) => {
+    return /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  };
+
+  const validator = (email: string) => {
+    const isValidEmail = isEmail(email);
+    if (isValidEmail) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+
+  return (
+    <Box sx={{ marginBottom: "60px" }}>
+      <Input
+        sx={{
+          border: `1px solid ${isValid ? "#b8c7ed" : "#ff5466"}`,
+          borderRadius: "2rem",
+          color: "#b8c7ed",
+          width: "400px",
+          padding: "0.25rem 0.5rem",
+          marginRight: "1rem",
+        }}
+        onChange={(e) => validator(e.target.value)}
+        placeholder="Your email address..."
+      />
+      {isValid ? null : (
+        <Typography sx={{ color: "#ff5466" }}>
+          Please provide a valid email address
+        </Typography>
+      )}
+      <Button
+        sx={{
+          backgroundColor: "#4c7bf3",
+          color: "#fff",
+          borderRadius: "2rem",
+          padding: "0.25rem 0.5rem",
+          width: "150px",
+          height: "40px",
+        }}
+      >
+        Notify Me
+      </Button>
+    </Box>
+  );
+};
+
+export default Email;
